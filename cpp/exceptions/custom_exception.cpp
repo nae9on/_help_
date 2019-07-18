b0e8036cfea52b645cbb3751f05aaaf7798b56c6
Ali Kadar <a.h.kadar@gmail.com>
2019-07-18 20:33:01 +0200

    custom exception

---
+++ cpp/exceptions/custom_exception.cpp
@@ -0,0 +1,36 @@
+/*
+ * custom_exception.cpp
+ *
+ *  Created on: Jul 18, 2019
+ *      Author: akadar
+ */
+
+#include <iostream>
+#include <exception>
+
+class my_exception: public std::exception
+{
+public:
+	my_exception(const char* str): message{str}{}
+
+	// what() is a virtual function in the base exception class
+	// note the const noexcept qualifiers
+	const char*	what() const noexcept{
+		std::cout<<"Exception thrown from my_exception class\n";
+		return message;
+	}
+private:
+	const char* message;
+};
+
+int main(){
+
+	try{
+		throw my_exception("dummy exception thrown");
+	}
+	catch(std::exception& e){
+		std::cout<<e.what();
+	}
+
+	return 0;
+}
