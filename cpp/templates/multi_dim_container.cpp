f91f10b09822ff0d26b101a400370fd222f45dac
Ali Kadar <a.h.kadar@gmail.com>
2019-08-03 02:05:03 +0200

    added multi_dim_container.cpp example in templates

--- cpp/templates/multi_dim_container.cpp
+++ cpp/templates/multi_dim_container.cpp
@@ -7,23 +7,69 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <assert.h>
+#include <ostream>
 
 template<size_t dim_, typename T> class multivec{
 public:
-	multivec(std::vector<size_t> size_): dim{dim_}, size{size_[dim_]} {
+	multivec(): dim{dim_}, size{0} {
 	};
-
+	void resize(std::deque<size_t> size_){
+		assert(!(dim_-size_.size()) && "size dimension mismatch");
+		size = size_[0];
+		data.resize(size_[0]);
+		std::cout<<dim_<<"D vector resized to "<<size<<"\n";
+		size_.pop_front();
+		for(size_t i=0; i<size; i++){
+			data[i].resize(size_);
+		}
+	}
+	void show(){
+		for(size_t i=0; i<size; i++){
+			data[i].show();
+			std::cout<<"\n";
+		}
+	}
+	multivec<dim_-1,T> operator[](size_t i) const{
+		return data[i];
+	}
+	multivec<dim_-1,T>& operator[](size_t i) {
+		return data[i];
+	}
+	size_t getSize(){
+		return size;
+	}
 private:
 	size_t dim;
 	size_t size;
-	multivec<dim_-1,T> data;
+	std::vector<multivec<dim_-1,T>> data;
 };
 
 template<typename T> class multivec<1,T>{
 public:
-	multivec(std::vector<size_t> size_): dim{1}, size{size_[1]} {
-		data.resize(size_[1]);
+	multivec(): dim{1}, size{0} {
 	};
+	void resize(std::deque<size_t> size_){
+		assert(!(1-size_.size()) && "size dimension mismatch");
+		size = size_[0];
+		data.resize(size_[0],0);
+		std::cout<<"1D vector resized to "<<size<<"\n";
+	}
+	void show(){
+		for (const auto& e : data){
+			std::cout<<e<<" ";
+		}
+	}
+	T operator[](size_t i) const{
+		return data.at(i);
+	}
+	T& operator[](size_t i){
+		return data.at(i);
+	}
+	size_t getSize(){
+		return size;
+	}
 
 private:
 	size_t dim;
@@ -31,12 +77,38 @@ private:
 	std::vector<T> data;
 };
 
-int main(){
-	std::cout<<"In main\n";
-	const int dim = 1;
-	std::vector<size_t> size{1};
 
-	multivec<dim,double> vec(size);
-	std::cout<<"End of main\n";
+template<typename T, size_t dim_> std::ostream& operator<<(std::ostream& os, multivec<dim_,T> e){
+	for(size_t i=0; i<e.getSize(); i++){
+		if (dim_==1)
+			os << e[i] << " ";
+		else
+			os << e[i];
+	}
+	os << "\n";
+}
+
+int multi_dim_container(){
+	std::cout<<"In main\n";
+
+	std::deque<size_t> size1{5};
+	std::deque<size_t> size2{3,5};
+	std::deque<size_t> size3{2,3,5};
+
+	multivec<1,double> vec1;
+	multivec<2,double> vec2;
+	multivec<3,double> vec3;
+
+	vec1.resize(size1);
+	vec2.resize(size2);
+	vec3.resize(size3);
+
+	vec1[4] = 11;
+	vec2[1][2] = 17;
+	vec3[1][0][2] = 7;
+
+	std::cout << vec3;
+
+	std::cout<<"\nEnd of main\n";
 	return 0;
 }
