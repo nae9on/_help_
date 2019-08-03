10b7fa01fbfd455a9294cfef425df4a91d9742b0
Ali Kadar <a.h.kadar@gmail.com>
2019-08-03 14:47:42 +0200

    multi-dimensional container example in templates

--- cpp/templates/multi_dim_container.cpp
+++ cpp/templates/multi_dim_container.cpp
@@ -3,6 +3,8 @@
  *
  *  Created on: Aug 1, 2019
  *      Author: akadar
+ *
+ * A multi-dimensional container implementation using templates.
  */
 
 #include <iostream>
@@ -13,77 +15,67 @@
 
 template<size_t dim_, typename T> class multivec{
 public:
+	using value_type = multivec<dim_-1,T>;
 	multivec(): dim{dim_}, size{0} {
 	};
 	void resize(std::deque<size_t> size_){
 		assert(!(dim_-size_.size()) && "size dimension mismatch");
 		size = size_[0];
-		data.resize(size_[0]);
+		data.resize(size);
 		std::cout<<dim_<<"D vector resized to "<<size<<"\n";
 		size_.pop_front();
 		for(size_t i=0; i<size; i++){
 			data[i].resize(size_);
 		}
 	}
-	void show(){
-		for(size_t i=0; i<size; i++){
-			data[i].show();
-			std::cout<<"\n";
-		}
-	}
-	multivec<dim_-1,T> operator[](size_t i) const{
+	value_type operator[](size_t i) const{
 		return data[i];
 	}
-	multivec<dim_-1,T>& operator[](size_t i) {
+	value_type& operator[](size_t i) {
 		return data[i];
 	}
-	size_t getSize(){
+	size_t getSize() const{
 		return size;
 	}
 private:
 	size_t dim;
 	size_t size;
-	std::vector<multivec<dim_-1,T>> data;
+	std::vector<value_type> data;
 };
 
+// partial template specialization
 template<typename T> class multivec<1,T>{
 public:
+	using value_type = T;
 	multivec(): dim{1}, size{0} {
 	};
 	void resize(std::deque<size_t> size_){
 		assert(!(1-size_.size()) && "size dimension mismatch");
 		size = size_[0];
-		data.resize(size_[0],0);
+		data.resize(size,0);
 		std::cout<<"1D vector resized to "<<size<<"\n";
 	}
-	void show(){
-		for (const auto& e : data){
-			std::cout<<e<<" ";
-		}
+	value_type operator[](size_t i) const{
+		return data[i];
 	}
-	T operator[](size_t i) const{
-		return data.at(i);
+	value_type& operator[](size_t i){
+		return data[i];
 	}
-	T& operator[](size_t i){
-		return data.at(i);
-	}
-	size_t getSize(){
+	size_t getSize() const{
 		return size;
 	}
-
 private:
 	size_t dim;
 	size_t size;
-	std::vector<T> data;
+	std::vector<value_type> data;
 };
 
-
-template<typename T, size_t dim_> std::ostream& operator<<(std::ostream& os, multivec<dim_,T> e){
-	for(size_t i=0; i<e.getSize(); i++){
+template<size_t dim_, typename T> std::ostream& operator<<(std::ostream& os, multivec<dim_,T> elem){
+	for(size_t i=0; i<elem.getSize(); i++){
 		if (dim_==1)
-			os << e[i] << " ";
+			os << elem[i] << " ";
 		else
-			os << e[i];
+			os << elem[i];
 	}
 	os << "\n";
 }
@@ -107,8 +99,17 @@ int multi_dim_container(){
 	vec2[1][2] = 17;
 	vec3[1][0][2] = 7;
 
-	std::cout << vec3;
+	std::cout << "\n" << vec3;
 
-	std::cout<<"\nEnd of main\n";
+	std::cout << vec3[1][0];
+
+	// Note that the default copy-constructor works fine even though the class multivec
+	// is a resource handle.
+	// The default copy-constructor will do a shallow-copy and since the underlying data
+	// container is a std::vector, the shallow-copy invokes the copy-constructor of the
+	// std::vector container which will do a deep-copy internally.
+	multivec<3,double> vec4 = vec3;
+
+	std::cout<<"End of main\n";
 	return 0;
 }
