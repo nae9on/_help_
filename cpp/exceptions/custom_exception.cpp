be3426705851c12e7ef66e3177dae190531b5eae
Ali Kadar <a.h.kadar@gmail.com>
2019-09-06 18:52:33 +0200

    minor update in exception

--- cpp/exceptions/custom_exception.cpp
+++ cpp/exceptions/custom_exception.cpp
@@ -11,8 +11,12 @@
 class my_exception: public std::exception
 {
 public:
-	my_exception(const char* str): message{str}{}
+	my_exception(){
+		message = "Default exception from my_exception";
+	}
+	my_exception(const char* str): message{str}{
 
+	}
 	// what() is a virtual function in the base exception class
 	// note the const noexcept qualifiers
 	const char*	what() const noexcept{
@@ -23,13 +27,46 @@ private:
 	const char* message;
 };
 
-int main_custom(){
+class derived_exception: public my_exception {
+public:
+	derived_exception(){
+		message = "Default exception from derived_exception";
+	}
+	derived_exception(const char* str): message{str}{
+
+	}
+	const char*	what() const noexcept{
+		std::cout<<"Exception thrown from derived_exception class\n";
+		return message;
+	}
+private:
+	const char* message;
+
+};
+
+int main(){
 
 	try{
-		throw my_exception("dummy exception thrown");
+		throw my_exception("dummy exception thrown from my_exception");
 	}
-	catch(std::exception& e){
-		std::cout<<e.what();
+	catch(const std::exception& e){
+		std::cout<<e.what()<<"\n";
+	}
+
+	/*
+	 * Note: Its a good practice to catch the exceptions by const& for the following two reasons:
+	 * 1. A reference allows one to access the exception polymorphically.
+	 * 2. A const increases performance by providing extra info to the compiler.
+	 * The latter is not as much important as the former, but the only real reason to drop const
+	 * would be to change the exception (usually useful only if one want's to rethrow it with
+	 * added context into a higher level).
+	 */
+
+	try{
+		throw derived_exception("dummy exception thrown from derived_exception");
+	}
+	catch(const my_exception& e){
+			std::cout<<e.what()<<"\n";
 	}
 
 	return 0;
