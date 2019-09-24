ee8a1ea675a7a4e3a37829222b783ee5c3e21597
Ali Kadar <a.h.kadar@gmail.com>
2019-09-24 19:56:34 +0200

    minor

--- cpp/containers/sequence/array/simple_array.cpp
+++ cpp/containers/sequence/array/simple_array.cpp
@@ -8,7 +8,9 @@
  * std::array is the simplest and was introduced in C++11. std::array container has some
  * advantages over a C-style array (int a[]). Its interface makes it more convenient to
  * find its size, and use with STL-style iterator-based algorithms. At the same time it
- * is equally fast. Note that std::array container allocates memory on the stack.
+ * is equally fast. The most important advantage of std::array over C-style array is that
+ * it saves users from surprising and nasty implicit conversions to pointer.
+ * Note that std::array container allocates memory on the stack.
  * References
  * [1] http://www.cplusplus.com/reference/array/array/
  * [2] https://stackoverflow.com/questions/30263303/stdarray-vs-array-performance
@@ -36,7 +38,26 @@
 #include <array>
 #include <valarray>
 
-int simple_array(){
+class Shape{
+public:
+	virtual void show() const = 0;
+	virtual ~Shape(){}
+private:
+};
+
+class Circle: public Shape{
+public:
+	void show() const {
+		std::cout<<"In Circle, y = "<<y<<"\n";
+	}
+	int getCircle(){
+		return y;
+	}
+private:
+	int y;
+};
+
+int main(){
 
 	std::array<double,10> arr1;
 	for (size_t i = 0; i<10; i++) arr1[i] = i;
@@ -45,6 +66,16 @@ int simple_array(){
 	arr2.resize(10);
 	for (size_t i = 0; i<10; i++) arr2[i] = i;
 	arr2+=10;
+
+	Circle arry[3];
+	std::array<Circle,3> arr2y;
+
+	Shape* ptr = arry;
+	ptr->show(); // OK, ptr points to arry[0]
+	(ptr+1)->show(); // disaster, because size(Circle)>size(Shape) and ptr[1] does not point to arry[1]
+	(ptr+2)->show(); // disaster, because size(Circle)>size(Shape) and ptr[2] does not point to arry[2]
+
+	// Shape* ptr2 = arr2y; // Error no implicit conversion, disaster prevented.
 
 	return 0;
 }
