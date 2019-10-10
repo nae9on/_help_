5a07c5e053dc01753a7ca58e5a6798306c1fd8c3
Ali Kadar <a.h.kadar@gmail.com>
2019-10-10 20:08:37 +0200

    minor

--- cpp/containers/erase_remove.cpp
+++ cpp/containers/erase_remove.cpp
@@ -4,11 +4,35 @@
  *  Created on: Sep 5, 2019
  *      Author: akadar
  *
- * Simple program which provides templated erase_remove for the following containers:
- * valarray, vector, deque, forward_list, list, map, set
+ * Simple program which provides templated erase_remove for the following STL containers:
+ * vector, deque, string, forward_list, list, map and set.
+ *
+ * All the elements of a container which satisfy a predicate condition must be removed.
+ *
+ * STL: how containers, iterators and algorithms collaborate?
+ * A container knows how the elements are organized in memory. An algorithm knows how to
+ * access elements of a container using iterator's. Note that there is no way to know
+ * the container type from the iterator type. Therefore, an algorithm can modify the value
+ * of elements of a container but cannot allocate/deallocate memory. This separation of roles
+ * is essential for the working of STL.
+ *
+ * erase remove idiom
+ *
+ * remove algorithm doesn't actually delete elements from a container. It only shifts non-deleted
+ * elements forward on top of deleted elements. The key is to realize that remove is designed to
+ * work on not a container but on any arbitrary forward iterator pair. That means it can't actually
+ * delete the elements (for the reason mentioned above).
+ *
+ * A call to remove is typically followed by a call to a container's erase method, which
+ * erases the unspecified values and reduces the physical size of the container to match
+ * its new logical size.
+ *
+ * Note: similar to remove, unique is also usually followed by a call to erase.
+ * Note: erase invalidates iterators and references at and after the point of the erase,
+ * including the end() iterator.
  *
  * References:
- * Meyers, Scott (2001). Effective STL: 50 Specific Ways to Improve ...
+ * Meyers, Scott (2001). Effective STL: 50 Specific Ways to Improve ...Scott#9,#32,#44
  * https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
  *
  */
@@ -16,107 +40,72 @@
 #include <iostream>
 #include <algorithm>
 
-#include <valarray>
 #include <vector>
 #include <deque>
+#include <string>
 #include <forward_list>
 #include <list>
 
 #include <map>
 #include <set>
 
-// Applicable for vector, deque, list
-template<typename T, template<typename> class A, template<typename,typename> class C>
-void erase_remove(C<T,A<T>>& ct){
-	/*
-	 * STL: how containers, iterators and algorithms collaborate?
-	 * A container knows how the elements are organized in memory.
-	 * An algorithm knows how to access elements of a container using an iterator.
-	 * An algorithm can modify the value of elements of a container but cannot
-	 * allocate/deallocate memory. This separation of roles is essential for the
-	 * working of STL.
-	 *
-	 * erase remove idiom
-	 *
-	 * remove doesn't actually delete elements from the container. It only shifts non-deleted
-	 * elements forward on top of deleted elements. The key is to realize that remove() is
-	 * designed to work on not a container but on any arbitrary forward iterator pair:
-	 * that means it can't actually delete the elements, because an arbitrary iterator pair
-	 * doesn't necessarily have the ability to delete elements.
-	 *
-	 * A call to remove is typically followed by a call to a container's erase method, which
-	 * erases the unspecified values and reduces the physical size of the container to match
-	 * its new logical size.
-	 *
-	 * Note: similar to remove, unique is also usually followed by a call to erase.
-	 * Note: erase invalidates iterators and references at and after the point of the erase,
-	 * including the end() iterator.
-	 *
-	 */
+// Predicate class for the function object
+class isOdd{
+public:
+	template<typename T>
+	bool operator()(const T& elem){
+		return elem%2;
+	}
+};
 
-	using IT = typename C<T,A<T>>::iterator;
-	IT begin_it = ct.begin();
-	IT end_it = ct.end();
-	IT it = std::remove_if(begin_it,end_it,[](const int& x)->bool{return x%2;});
-	ct.erase(it, ct.end());
-}
-
-// partial template specialization for valarray
+// Applicable for vector, deque, string
 template<typename T>
-void erase_remove(std::valarray<T>& ct){
-	using IT = T*;
-	IT begin_it = std::begin(ct);
-	IT end_it = std::end(ct);
-	IT it = std::remove_if(begin_it,end_it,[](const int& x)->bool{return x%2;});
-
-	// Perhaps this can be improved?
-	std::valarray<T> ct2;
-	ct2.resize(it-begin_it,0);
-	std::copy(begin_it,it,std::begin(ct2));
-	ct.resize(it-begin_it,0);
-	std::copy(std::begin(ct2),std::end(ct2),std::begin(ct));
+void erase_remove(T& ct){
+	auto it = std::remove_if(ct.begin(),ct.end(),(isOdd()));
+	ct.erase(it, ct.end());
+	// ct.erase_if is available in C++20!
 }
 
-// partial template specialization for forward_list
-template<typename T, template<typename> class A>
-void erase_remove(std::forward_list<T,A<T>>& ct){
-	using IT = typename std::forward_list<T,A<T>>::iterator;
-	IT begin_it = ct.begin();
-	IT end_it = ct.end();
-	IT it = std::remove_if(begin_it,end_it,[](const int& x)->bool{return x%2;});
-	ct.erase_after(it, ct.cend());
+// Partial template specialization for list (also applicable to forward_list)
+template<typename T>
+void erase_remove(std::list<T>& ct){
+	ct.remove_if((isOdd()));
+	// ct.erase_if is available in C++20!
+	// ct.erase_if and ct.remove_if are equivalent for a list
 }
 
 // partial template specialization for map
 // map is a node based container and therefore erasing an element is straight forward
 template<typename K, typename V>
 void erase_remove(std::map<K,V>& ct){
-	using IT = typename std::map<K,V>::iterator;
-	IT begin_it = ct.begin();
-	IT end_it = ct.end();
-	for(IT it = begin_it; it!=end_it; ++it){
-		if(it->first%2){
+	for(auto it = ct.begin(); it!=ct.end();){
+		if(isOdd()(it->first)){
 			// References and iterators to the erased elements are invalidated.
-			// Other references and iterators are not affected.
-			ct.erase(it);
+			// Other references and iterators are not affected. Scott#9
+			ct.erase(it++);
+		}else
+		{
+			++it;
 		}
 	}
+	// ct.erase_if is available in C++20!
 }
 
 // partial template specialization for set
 // set is a node based container and therefore erasing an element is straight forward
 template<typename K>
 void erase_remove(std::set<K>& ct){
-	using IT = typename std::set<K>::iterator;
-	IT begin_it = ct.begin();
-	IT end_it = ct.end();
-	for(IT it = begin_it; it!=end_it; ++it){
-		if(*it%2){
+	for(auto it = ct.begin(); it!=ct.end();){
+		if(isOdd()(*it)){
 			// References and iterators to the erased elements are invalidated.
-			// Other references and iterators are not affected.
-			ct.erase(it);
+			// Other references and iterators are not affected. Scott#9
+			ct.erase(it++);
+		}else
+		{
+			++it;
 		}
 	}
+	// ct.erase_if is available in C++20!
 }
 
 template<typename T>
@@ -136,25 +125,10 @@ void print_container(const std::string& s, std::map<K,V>& ct){
 
 int erase_remove(){
 
-	std::valarray<int> varr({3,1,2,6,4,8,9,10});
-	print_container("Before erase remove, varr = ",varr);
-	erase_remove(varr);
-	print_container("After erase remove, varr = ",varr);
-
 	std::vector<int> vec({3,1,2,6,4,8,9,10});
 	print_container("Before erase remove, vec = ",vec);
 	erase_remove(vec);
 	print_container("After erase remove, vec = ",vec);
-
-	std::deque<int> deq({3,1,2,6,4,8,9,10});
-	print_container("Before erase remove, deq = ",deq);
-	erase_remove(deq);
-	print_container("After erase remove, deq = ",deq);
-
-	std::forward_list<int> flist({3,1,2,6,4,8,9,10});
-	print_container("Before erase remove, flist = ",flist);
-	erase_remove(flist);
-	print_container("After erase remove, flist = ",flist);
 
 	std::list<int> llist({3,1,2,6,4,8,9,10});
 	print_container("Before erase remove, llist = ",llist);
