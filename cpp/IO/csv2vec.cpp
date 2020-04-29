9527683da80e13323df98f4e851f8b438204d2a6
a.h.kadar <a.h.kadar@gmail.com>
2020-04-29 11:30:50 +0200

    minor in cpp

--- cpp/IO/csv2vec.cpp
+++ cpp/IO/csv2vec.cpp
@@ -18,37 +18,37 @@
 
 template<typename In, typename Out>
 void csv2vec(In first, In last, Out& result) {
-	std::string temp;
-	unsigned i{0};
-	while(1) {
-		do{
-			if(*first == '\n') break;
-			temp.push_back(*first);
-			++first;
-		}while(*first != ',' && first != last);
-		result[i].push_back(temp);
-		if(*first == '\n') {
-			++i;
-			result.resize(i+1);
-		}
-		temp.clear();
-		if(first == last) break;
-		++first;
-	}
+    std::string temp;
+    unsigned i{0};
+    while(1) {
+        do{
+            if(*first == '\n') break;
+            temp.push_back(*first);
+            ++first;
+        }while(*first != ',' && first != last);
+        result[i].push_back(temp);
+        if(*first == '\n') {
+            ++i;
+            result.resize(i+1);
+        }
+        temp.clear();
+        if(first == last) break;
+        ++first;
+    }
 }
 
-int csv2vec(void) {
-	std::vector<std::vector<std::string>> vec(1);
-	std::ifstream in("data.txt");
-	std::istream_iterator<char> begin(in);
-	std::istream_iterator<char> end;
-	csv2vec(begin,end,vec);
+int main(void) {
+    std::vector<std::vector<std::string>> vec(1);
+    std::ifstream in("data.txt");
+    std::istreambuf_iterator<char> begin(in);
+    std::istreambuf_iterator<char> end;
+    csv2vec(begin,end,vec);
 
-	for(const auto& vecouter : vec) {
-		for(const auto& elem : vecouter){
-			std::cout<<elem<<" ";
-		}
-		std::cout<<"\n";
-	}
+    for(const auto& vecouter : vec) {
+        for(const auto& elem : vecouter){
+            std::cout<<elem<<" ";
+        }
+        std::cout<<"\n";
+    }
    return 0;
 }
