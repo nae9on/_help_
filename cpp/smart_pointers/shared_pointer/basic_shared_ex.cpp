258f338c886ed318400d1e416e56fa11f3ae968d
a.h.kadar <a.h.kadar@gmail.com>
2020-06-07 18:03:26 +0200

    updated smart pointers

--- cpp/smart_pointers/shared_pointer/basic_shared_ex.cpp
+++ cpp/smart_pointers/shared_pointer/basic_shared_ex.cpp
@@ -7,28 +7,101 @@
 
 #include <iostream>
 #include <memory>
-
-class X1{};
+#include <boost/type_index.hpp>
 
 class Y{
 public:
-	Y(){std::cout<<"Y::ctor called\n";}
-	~Y(){std::cout<<"Y::dtor called\n";}
+    Y():data{0}{++count; std::cout<<"ctor called\n";};
+    Y(int data_):data{data_}{++count; std::cout<<"ctor called for data = "<<data_<<"\n";}
+    Y(const Y& x){
+        data = x.data;
+    }
+    void outData()const{std::cout<<"output data = "<<data<<"\n";}
+    static int getCount(){return count;}
+    ~Y(){--count; std::cout<<"dtor called for data = "<<data<<"\n";}
+private:
+    int data;
+    static int count;
 };
+
+int Y::count = 0;
+
+void showcount(const std::shared_ptr<Y>& sptr){
+    std::cout<<"Use count = "<<sptr.use_count()<<"\n";
+}
 
 int basic_shared_ex(){
 
-	std::unique_ptr<X1> uptr(new X1);
-	//std::unique_ptr<X1> uptr_copy(uptr); // Error
+    // ways to initialize shared_ptr's
+    {
+        // 1. not recommended
+        Y* yptr = new Y(12);
+        std::shared_ptr<Y> sptr(yptr);
+        //std::shared_ptr<Y> sptr2(yptr); // disaster
 
-	std::shared_ptr<Y> sptr(new Y); // counter = 1
-	std::shared_ptr<Y> sptr_copy1(sptr); // OK, shared ownership, counter = 2
-	std::shared_ptr<Y> sptr_copy2; // owns nullptr
-	sptr_copy2 = sptr; // OK, shared ownership, counter = 3
+        // 2. recommended
+        auto sptr2{std::make_shared<Y>(34)}; // Note requires c++14 support
 
-	sptr_copy2.reset(); // counter = 2
-	sptr_copy1.reset(); // counter = 1
-	sptr.reset(); // counter = 0 and thus destructor called
+        // 3. ok
+        std::shared_ptr<Y> sptr3(new Y(56));
+
+        // 4. also ok
+        std::shared_ptr<Y> sptr4(nullptr); // Note: it is set to nullptr when not initialized
+        //sptr4 = new Y(78); // Error Implicit conversion from raw pointer to shared_ptr is not allowed
+        sptr4.reset(new Y(78));
+        std::cout<<"End of block\n";
+    }
+
+    std::cout<<"***************************\n";
+
+    // using shared pointer for shared ownership
+    {
+        auto sptr{std::make_shared<Y>(10)};
+
+        // Note size of std::shared_ptr is = size of two pointers (one for
+        // the internal raw pointer and the other for the control block)
+        std::cout<<boost::typeindex::type_id_with_cvr<decltype(sptr)>()<<"\n";
+        std::cout<<"Size of shared_ptr = "<<sizeof(decltype(sptr))<<"\n";
+
+        // each resoure has its own unique control block which keeps count of the
+        // number of owners of that resource
+        showcount(sptr); // counter = 1
+
+        std::shared_ptr<Y> sptr_copy1(sptr); // OK, shared ownership, counter = 2
+        showcount(sptr); // counter = 2, same as showcount(sptr_copy1);
+
+        std::shared_ptr<Y> sptr_copy2; // owns nullptr
+        sptr_copy2 = sptr; // OK, shared ownership
+        showcount(sptr); // counter = 3
+
+        sptr_copy2.reset();
+        showcount(sptr); // counter = 2
+
+        sptr_copy1.reset();
+        showcount(sptr); // counter = 1
+
+        sptr.reset();
+        showcount(sptr); // counter = 0
+        std::cout<<"End of block\n";
+    }
+
+    std::cout<<"***************************\n";
+
+    // move semantics
+    {
+        std::shared_ptr<Y> sptr1(new Y(33)), sptr2(new Y(99)), sptr3;
+
+        sptr1 = sptr3; // Y(33) not owned by anyone and therefore destroyed
+        //showcount(sptr1); showcount(sptr2); showcount(sptr3);
+
+        sptr3 = sptr2; // ownership  of Y(99) shared
+        //sptr3 = std::move(sptr2); // ownership  of Y(99) transferred
+        showcount(sptr1); showcount(sptr2); showcount(sptr3);
+
+        std::cout<<"End of block\n";
+    }
+
+    std::cout<<"***************************\n";
 
 	std::cout<<"End main\n";
 	return 0;
--- cpp/smart_pointers/unique_pointer/basic_unique_ex.cpp
+++ cpp/smart_pointers/unique_pointer/basic_unique_ex.cpp
@@ -8,17 +8,19 @@
 #include <iostream>
 #include <memory>
 #include <boost/type_index.hpp>
+#include <vector>
+#include <algorithm>
 
 class X{
 public:
 	X():data{0}{++count; std::cout<<"ctor called\n";};
-	X(int data_):data{data_}{++count; std::cout<<"ctor called\n";}
+	X(int data_):data{data_}{++count; std::cout<<"ctor called for data = "<<data_<<"\n";}
 	X(const X& x){
 		data = x.data;
 	}
-	void outData()const{std::cout<<"data = "<<data<<"\n";}
+	void outData()const{std::cout<<"output data = "<<data<<"\n";}
 	static int getCount(){return count;}
-	~X(){--count; std::cout<<"dtor called\n";}
+	~X(){--count; std::cout<<"dtor called for data = "<<data<<"\n";}
 private:
 	int data;
 	static int count;
@@ -26,13 +28,23 @@ private:
 
 int X::count = 0;
 
+void scanvec(std::vector<std::unique_ptr<X>>& uptrvec){
+    for(const auto& elem:uptrvec){
+        if(elem==nullptr)
+            std::cout<<"nullptr"<<"\t";
+        else
+            std::cout<<"notnullptr"<<"\t";
+    }
+    std::cout<<std::endl;
+}
+
 int basic_unique_ex(){
 
-	// using C++98 style new and delete
+    // using raw pointers
 	{
-		auto void_ptr = new X(10);
-		std::cout<<boost::typeindex::type_id_with_cvr<decltype(void_ptr)>()<<"\n";
-		X* ptr{void_ptr};
+		auto rptr{new X(10)};
+		std::cout<<boost::typeindex::type_id_with_cvr<decltype(rptr)>()<<"\n";
+		X* ptr{rptr};
 		std::cout<<"Size of raw pointer = "<<sizeof(decltype(ptr))<<"\n";
 		ptr->outData();
 		delete ptr;
@@ -40,14 +52,52 @@ int basic_unique_ex(){
 
 	std::cout<<"***************************\n";
 
-	// using smart pointers
+	// ways to initialize unique_ptr's
 	{
-		std::unique_ptr<X> sptr(nullptr);
-		//sptr = new X(10); // Error implicit conversion from raw to unique_ptr is not allowed
-		sptr.reset(new X(10));
-		std::cout<<"Size of unique_ptr = "<<sizeof(decltype(sptr))<<"\n";
-		sptr->outData();
-		(*sptr).outData();
+	    // 1. not recommended
+	    X* xptr = new X(12);
+	    std::unique_ptr<X> uptr(xptr);
+	    //std::unique_ptr<X> uptr2(xptr); // disaster
+
+	    // 2. recommended
+	    auto uptr2{std::make_unique<X>(34)}; // Note requires c++14 support
+
+	    // 3. ok
+	    std::unique_ptr<X> uptr3(new X(56));
+
+	    // 4. also ok
+	    std::unique_ptr<X> uptr4(nullptr); // Note: it is set to nullptr when not initialized
+	    //uptr4 = new X(789); // Error Implicit conversion from raw pointer to unique_ptr is not allowed
+	    uptr4.reset(new X(78));
+	    std::cout<<"End of block\n";
+	}
+
+	std::cout<<"***************************\n";
+
+	// using unique pointer for exclusive ownership
+	{
+	    auto uptr1{std::make_unique<X>(10)};
+	    std::unique_ptr<X> uptr2;
+		std::cout<<"Size of unique_ptr = "<<sizeof(decltype(uptr1))<<"\n";
+
+		// unique_ptr's are move only and cannot be copied
+		//uptr2 = uptr1; // Error
+		uptr2 = std::move(uptr1);
+		if(uptr1==nullptr) std::cout<<"uptr1 is now nullptr\n";
+
+		// unique_ptr is a wrapper around raw pointer and thus acts like one
+		uptr2->outData();
+		(*uptr2).outData();
+
+		// moving around std::unique_ptr
+		std::vector<std::unique_ptr<X>> uptrvec, uptrvec_copy;
+		uptrvec.push_back(std::move(uptr1));
+		uptrvec.push_back(std::move(uptr2));
+		//std::copy(uptrvec.begin(), uptrvec.end(), std::back_inserter(uptrvec_copy)); // Error
+		std::move(uptrvec.begin(), uptrvec.end(), std::back_inserter(uptrvec_copy));
+		scanvec(uptrvec);
+		scanvec(uptrvec_copy);
+		std::cout<<"End of block\n";
 	}
 
 	std::cout<<"***************************\n";
@@ -56,9 +106,10 @@ int basic_unique_ex(){
 	// std::array and std::vector should be preferred over array form
 	// useful only when taking ownership of an array returned by a C-like API (Item 18, EMC++)
 	{
-		std::unique_ptr<X[]> sptr_ar(new X[3]);
-		std::cout<<"Size of array form of unique_ptr = "<<sizeof(decltype(sptr_ar))<<"\n";
-		//sptr_ar->outData(); // Error op* and op-> are not supported, only operator[] is supported
+		std::unique_ptr<X[]> uptr_ar(new X[3]);
+		std::cout<<"Size of array form of unique_ptr = "<<sizeof(decltype(uptr_ar))<<"\n";
+		//uptr_ar->outData(); // Error op* and op-> are not supported, only operator[] is supported
+		std::cout<<"End of block\n";
 	}
 
 	std::cout<<"Current count = "<<X::getCount()<<"\n";
