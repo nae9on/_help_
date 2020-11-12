dbbdf8383e6815d0e75cebc808e6109738d3b858
A.H. Kadar <31695244+nae9on@users.noreply.github.com>
2020-11-12 19:42:20 +0100

    Create string_view.cpp

---
+++ cpp/strings/string_view.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+std::string_view GetExtension(const std::string &Path)
+{
+    auto last_period_idx = Path.find_last_of(".");
+    if(last_period_idx == std::string::npos) { last_period_idx = 0; }
+    return {&Path[last_period_idx], Path.size()-last_period_idx}; // file has no extension
+}
+
+int main()
+{
+  std::cout << GetExtension("dir\\dir\\dir\\") << "\n";
+  std::cout << GetExtension("dir\\dir\\dir\\file.ext") << "\n";
+}
