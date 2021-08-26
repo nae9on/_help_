b457190329e03bfeed4ec1889bcf9e935334a129
A.H. Kadar <31695244+nae9on@users.noreply.github.com>
2021-08-26 10:53:20 +0200

    Update regex_replace.cpp

--- cpp/regex_examples/regex_replace.cpp
+++ cpp/regex_examples/regex_replace.cpp
@@ -1,1 +1,28 @@
+// Example program
+#include <regex>
+#include <string>
+#include <iostream>
 
+int main()
+{
+    std::string RawString= R"(<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+                                  <xs:element name="Message" type="MessageType"/>
+                                  <xs:complexType name="MessageType">
+                                      <xs:all>
+                                          <xs:element name="Body" type="BodyType"/>
+                                      </xs:all>
+                                      <xs:attribute name="Type" type="xs:string" fixed="GetBackgroundImage"/>
+                                  </xs:complexType>
+                              </xs:schema>)";
+
+
+    std::regex Pattern{">\\s*<"}; // Replace zero or more whitespace characters between ><
+    std::string ReplacementText {"><"};
+    std::string Constraints;
+    std::regex_replace(std::back_inserter(Constraints), RawString.begin(), RawString.end(), Pattern, ReplacementText);
+    std::string Expected = R"(<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"><xs:element name="Message" type="MessageType"/><xs:complexType name="MessageType"><xs:all><xs:element name="Body" type="BodyType"/></xs:all><xs:attribute name="Type" type="xs:string" fixed="GetBackgroundImage"/></xs:complexType></xs:schema>)";
+    if(Expected==Constraints)
+    {
+        std::cout << "Success";
+    }    
+}
