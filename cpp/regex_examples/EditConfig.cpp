54f12912f19aee3b4bb1bfcd9b9730c9eb729d39
A.H. Kadar <31695244+nae9on@users.noreply.github.com>
2021-11-05 21:01:57 +0100

    Create EditConfig.cpp

---
+++ cpp/regex_examples/EditConfig.cpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <regex>
+#include <sstream>
+#include <string>
+
+class CEditConfigXML
+{
+public:
+    explicit CEditConfigXML(const std::string& Config)
+        : m_Config{Config}
+    {}
+
+    template<typename T>
+    CEditConfigXML& ReplaceText(const std::string& ParameterName, const T& Value)
+    {
+        std::stringstream From, To;
+        From << ParameterName << "=" << "\".*\""; // ".*" wild card will match 0 or more characters
+        To << ParameterName << "=" << "\"" << Value << "\"";
+        std::regex Pattern{From.str()};
+        m_Config = std::regex_replace(m_Config, Pattern, To.str());
+        return *this;
+    }
+
+    const std::string& GetConfig() const
+    {
+        return m_Config;
+    }
+
+private:
+    std::string m_Config;
+};
