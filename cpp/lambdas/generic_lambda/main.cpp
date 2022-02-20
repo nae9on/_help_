0ed59640eb0ec65b827a1f54439f2e4b22a518d5
a.h.kadar <a.h.kadar@gmail.com>
2022-02-20 22:46:17 +0100

    comment in generic lambda

--- cpp/lambdas/generic_lambda/main.cpp
+++ cpp/lambdas/generic_lambda/main.cpp
@@ -5,6 +5,13 @@
  *      Author: akadar
  *
  *  Simple example to demonstrate the usage of generic lambdas.
+ *
+ *  Reference:
+ *  Section 11.4.5 The Type of a Lambda from "The C++ Programming Language 4th Edition"
+ *  The exact type of a lambda expression is not defined. No two lambda expressions can have the same type!!
+ *  The type of a lambda is roughly a local class type with a ctor and a const member function operator()()
+ *  Altough their exact type is not defined, lambda's can be used to initialize variables declared auto or
+ *  std::function<R<AL>> where R is the lambda's return type and AL is its argument list of types.
  */
 
 #include "transform.h"
